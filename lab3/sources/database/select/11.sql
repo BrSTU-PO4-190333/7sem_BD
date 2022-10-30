2fe6fdd7c64fdbb006036baafe7faebcf171b766
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-10-31 01:16:52 +0300

    lab3/190333 : Task 11 added

---
+++ lab3/sources/database/select/11.sql
@@ -0,0 +1,23 @@
+SELECT
+    A.de_doctorId AS ИдДоктора,
+    AVG(A.de_endTime - A.de_startTime) AS ОбщееВремяОбслуживанияПациентовВГоспитале,
+    C.de_surname AS ФамилияВрача,
+    C.de_name AS ИмяВрача,
+    C.de_patronymic AS ОтчествоВрача
+FROM
+    DE_DOC_Inspection AS A,
+    DE_CTL_InspectionPlaces AS B,
+    DE_CTL_Doctors AS C
+WHERE
+    A.de_placeId = B.id
+   AND
+   B.de_name = 'поликлиника'
+    AND
+    A.de_doctorId = C.id
+GROUP BY
+    A.de_doctorId,
+    C.de_surname,
+    C.de_name,
+    C.de_patronymic
+ORDER BY ОбщееВремяОбслуживанияПациентовВГоспитале
+DESC;
