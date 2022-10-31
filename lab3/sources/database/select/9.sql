b08bfda4265da98d9844b8070a7966a9fb3ec90d
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-10-31 13:03:56 +0300

    lab3/190333 : Task 9 added

---
+++ lab3/sources/database/select/9.sql
@@ -0,0 +1,35 @@
+-- Чтобы выполнить это задание нужно добавить больше лекарств,
+-- чтобы была для пациентов количество не 1, а больше
+-- Иначе задание не показать наглядно
+-- Нужно добавить больше данных в БД
+
+-- SELECT
+--     MAX( (DATE_PART('year', NOW()) - DATE_PART('year', B.de_birthday)) )
+-- FROM
+--     DE_DOC_Inspection AS A,
+--     DE_CTL_Patients AS B
+-- WHERE
+--     A.de_patientId = B.id
+
+
+-- SELECT MAX(КоличествоПрописанныхЛекарств)
+-- FROM (
+--     SELECT 
+--     C.de_patientId AS ИдПациента,
+--     D.de_medicineId AS ИдЛекарства,
+--     E.de_name AS Лекарство,
+--         COUNT(E.de_name) AS КоличествоПрописанныхЛекарств
+--     FROM
+--         DE_DOC_Inspection AS C,
+--         DE_TAB_InspectionMedicines AS D,
+--         DE_CTL_Medicines AS E
+--     WHERE
+--         C.id = D.de_inspectionId
+--         AND
+--         D.de_medicineId = E.id
+--     GROUP BY
+--         C.de_patientId,
+--         D.de_medicineId,
+--         E.de_name
+--     ORDER BY ИдЛекарства
+-- ) AS F
