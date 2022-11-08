313566c5eeb6d8b19a28a1f6fc647c746b477fe8
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-11-08 22:25:20 +0300

    lab3/190333 : 1st task updated

--- lab3/sql/select/1.sql
+++ lab3/sql/select/1.sql
@@ -1,20 +1,43 @@
-SELECT 
-	A.de_startTime AS дата,
-	(A.de_endTime - A.de_startTime) AS продолжительность,
-	B.de_name AS место_осмотра,
-	CONCAT(C.de_surname, ' ', C.de_name, ' ', C.de_patronymic)  AS данные_врача,
-	CONCAT(D.de_birthday, ': ', D.de_name, ' ', D.de_name, ' ', D.de_patronymic, ', проживающий по адресу ', D.de_address) AS данные_пациента
+/*
+ Вывести данные о всех приемах (дату, продолжительность приема в минутах, место
+ осмотра, данные врача, данные пациента), которые были проведены между датами
+ 01.01.19 и 20.02.19 (привести два варианта решения задачи)
+ */
+
+SELECT
+    A.de_startTime AS дата,
+    date_part(
+        'minutes',
+        A.de_endTime - A.de_startTime
+    ) AS продолжительность_в_минутах,
+    B.de_name AS место_осмотра,
+    CONCAT(
+        C.de_surname,
+        ' ',
+        C.de_name,
+        ' ',
+        C.de_patronymic
+    ) AS данные_врача,
+    CONCAT(
+        D.de_birthday,
+        ': ',
+        D.de_name,
+        ' ',
+        D.de_name,
+        ' ',
+        D.de_patronymic,
+        ', проживающий по адресу ',
+        D.de_address
+    ) AS данные_пациента
 FROM
-	DE_DOC_Inspection AS A,
-	DE_CTL_InspectionPlaces AS B,
-	DE_CTL_Doctors AS C,
-	DE_CTL_Patients AS D
+    DE_DOC_Inspection AS A,
+    DE_CTL_InspectionPlaces AS B,
+    DE_CTL_Doctors AS C,
+    DE_CTL_Patients AS D
 WHERE
-	A.de_placeId = B.id
-	AND
-	A.de_patientId = D.id
-	AND
-	A.de_startTime >= '2019-01-01 00:00:00'
-	AND 
-	A.de_endTime <= '2019-02-20 00:00:00'
+    B.id = A.de_placeId
+    AND C.id = A.de_doctorId
+    AND D.id = A.de_patientId
+    AND A.de_startTime >= '2019-01-01 00:00:00'
+    AND A.de_endTime <= '2019-02-20 00:00:00'
 ORDER BY дата;
