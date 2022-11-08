8ea9c975629107fffaad79790246ed18960e06ea
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-11-08 23:28:15 +0300

    lab3/190333 : 5th task updated

--- lab3/README.md
+++ lab3/README.md
@@ -8,7 +8,7 @@
 - [x] задание 2 | [код](sql/select/2.sql)
 - [ ] задание 3 | [код](sql/select/3.sql)
 - [x] задание 4 | [код](sql/select/4.sql)
-- [ ] задание 5 | [код](sql/select/5.sql)
+- [x] задание 5 | [код](sql/select/5.sql)
 - [ ] задание 6 | [код](sql/select/6.sql)
 - [ ] задание 7 | [код](sql/select/7.sql)
 - [ ] задание 8 | [код](sql/select/8.sql)
--- lab3/sql/select/5.sql
+++ lab3/sql/select/5.sql
@@ -1,6 +1,54 @@
-SELECT id
-FROM de_ctl_medicines
+/*
+ Вывести диагнозы, которые не были поставлены ни одним врачом
+ = = = = = = = =
+ У нас 325 632 осмотра:
+ SELECT COUNT(*) AS КоличествоОсмотров
+ FROM DE_DOC_Inspection;
+ ---
+ У нас 14629 диагнозов
+ SELECT COUNT(*) FROM DE_CTL_Diagnosis;
+ ---
+ У нас из 325 632 осмотров выявлены 14629 уникальных дигнозов
+ SELECT COUNT(DISTINCT de_diagnosisId) FROM DE_DOC_Inspection;
+ ---
+ У нас все дигнозы за 3 года выявлены, поэтому выберим период в два месяца.
+ 11 724 осмотра за два месяца
+ SELECT COUNT(*)
+ FROM DE_DOC_Inspection
+ WHERE de_startTime >= '2019-01-01 00:00:00'
+ AND de_endTime <= '2019-02-20 00:00:00'
+ ---
+ 7826 уникальных диагнозов выявлено за 11 724 осмотра (два месяца)
+ SELECT COUNT(DISTINCT de_diagnosisId)
+ FROM DE_DOC_Inspection
+ WHERE de_startTime >= '2019-01-01 00:00:00'
+ AND de_endTime <= '2019-02-20 00:00:00'
+ ---
+ 14629-7826=6803
+ SELECT COUNT(*)
+ FROM DE_CTL_Diagnosis
+ WHERE id NOT IN (
+ SELECT de_diagnosisId
+ FROM
+ DE_DOC_Inspection
+ WHERE
+ de_startTime >= '2019-01-01 00:00:00'
+ AND de_endTime <= '2019-02-20 00:00:00'
+ );
+ ---
+ Выведим диагнозы, которые не поставили ниже  
+ */
+
+SELECT
+    id AS ИдДигнозаНеПоставленогоЗаПериод,
+    de_name AS НаименованиеДиагнозаНеПОставленыйЗаПериод,
+    de_description AS ОписаниеДиагнозаНеПоставленыйЗаПериод
+FROM DE_CTL_Diagnosis
 WHERE id NOT IN (
-    SELECT DISTINCT de_medicineid
-    FROM de_tab_inspectionmedicines
-);
+        SELECT de_diagnosisId
+        FROM
+            DE_DOC_Inspection
+        WHERE
+            de_startTime >= '2019-01-01 00:00:00'
+            AND de_endTime <= '2019-02-20 00:00:00'
+    );
