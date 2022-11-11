e63da6c04ed2023d59866dbfcc19850c9f02e845
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-11-12 01:35:23 +0300

    lab3/190333 : Sources added

--- lab3/sql/select/3.sql
+++ lab3/sql/select/3.sql
@@ -1,25 +1,53 @@
--- Перём максимальное число обслужаных пациентов на дому
-SELECT MAX(КолвоОблужаныхПациентовНаДому) FROM (
-    -- Запрос выводит список вращей и их количество пациентов обслужаных на дому
-    SELECT
-        A.de_doctorId AS ИдДоктора,
-        COUNT(A.de_patientId) AS КолвоОблужаныхПациентовНаДому,
-        C.de_surname AS ФамилияВрача,
-        C.de_name AS ИмяВрача,
-        C.de_patronymic AS ОтчествоВрача
-    FROM
-        DE_DOC_Inspection AS A,
-        DE_CTL_InspectionPlaces AS B,
-        DE_CTL_Doctors AS C
-    WHERE
-        A.de_placeId = B.id
-        AND
-        B.de_name = 'на дому'
-        AND
-        A.de_doctorId = C.id
-    GROUP BY
-        A.de_doctorId,
+SELECT
+    B.de_name AS МетстоОбслуживания,
+    COUNT(A.de_patientId) AS КолвоОблужаныхПациентовНаДому,
+    CONCAT(
+        'Участок №',
+        C.de_region,
+        ', кабинет ',
+        C.de_office,
+        ', ',
         C.de_surname,
+        ' ',
         C.de_name,
-        C.de_patronymic
-) AS Q;
+        ' ',
+        C.de_name
+    ) AS ДанныеВрача
+FROM
+    DE_DOC_Inspection AS A,
+    DE_CTL_InspectionPlaces AS B,
+    DE_CTL_Doctors AS C
+WHERE
+    A.de_placeId = B.id
+    AND A.de_doctorId = C.id
+    AND B.de_name = 'на дому'
+GROUP BY
+    B.de_name,
+    A.de_doctorId,
+    C.de_region,
+    C.de_office,
+    C.de_surname,
+    C.de_name,
+    C.de_patronymic
+HAVING
+    COUNT(A.de_patientId) = (
+        SELECT
+            MAX(
+                КолвоОблужаныхПациентовНаДому
+            )
+        FROM (
+                SELECT
+                    COUNT(A.de_patientId) AS КолвоОблужаныхПациентовНаДому
+                FROM
+                    DE_DOC_Inspection AS A,
+                    DE_CTL_InspectionPlaces AS B,
+                    DE_CTL_Doctors AS C
+                WHERE
+                    A.de_placeId = B.id
+                    AND A.de_doctorId = C.id
+                    AND B.de_name = 'на дому'
+                GROUP BY
+                    B.de_name,
+                    A.de_doctorId
+            ) AS Q
+    );
