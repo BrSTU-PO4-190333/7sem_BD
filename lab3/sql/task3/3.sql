e8df5425140207c4aeac8924cce46e9a597c16f5
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-11-21 00:32:11 +0300

    lab3/190333 : Sources added

--- lab3/sql/task3/3.sql
+++ lab3/sql/task3/3.sql
@@ -51,3 +51,55 @@ HAVING
                     A.de_doctorId
             ) AS Q
     );
+
+SELECT
+    -- = = = = = = = = = = = = = = = = Колонка "МестоОбслуживания"
+    Places2.de_name AS МестоОбслуживания,
+    -- = = = = = = = = = = = = = = = = Колонка "КоличествоОбслуженныхПациентов"
+    COUNT(Inspections2.de_patientId) AS КоличествоОбслуженныхПациентов,
+    -- = = = = = = = = = = = = = = = = Колонка "ДанныеВрача"
+    CONCAT(
+        'Участок №',
+        Doctors2.de_region,
+        ', кабинет ',
+        Doctors2.de_office,
+        ', ',
+        Doctors2.de_surname,
+        ' ',
+        Doctors2.de_name,
+        ' ',
+        Doctors2.de_patronymic
+    ) AS ДанныеВрача
+FROM
+    DE_DOC_Inspection AS Inspections2
+    INNER JOIN DE_CTL_InspectionPlaces AS Places2 ON Inspections2.de_placeId = Places2.id
+    INNER JOIN DE_CTL_Doctors AS Doctors2 ON Inspections2.de_doctorId = Doctors2.id
+WHERE Places2.de_name = 'на дому'
+GROUP BY
+    Places2.de_name,
+    Inspections2.de_doctorId,
+    Doctors2.de_region,
+    Doctors2.de_office,
+    Doctors2.de_surname,
+    Doctors2.de_name,
+    Doctors2.de_patronymic
+HAVING
+    COUNT(Inspections2.de_patientId) = (
+        SELECT
+            MAX(КолОблужПацНаДому)
+        FROM (
+                SELECT
+                    COUNT(Inspection.de_patientId) AS КолОблужПацНаДому
+                FROM
+                    DE_DOC_Inspection AS Inspection,
+                    DE_CTL_InspectionPlaces AS Places,
+                    DE_CTL_Doctors AS Doctors
+                WHERE
+                    Inspection.de_placeId = Places.id
+                    AND Inspection.de_doctorId = Doctors.id
+                    AND Places.de_name = 'на дому'
+                GROUP BY
+                    Places.de_name,
+                    Inspection.de_doctorId
+            ) AS АлиасПодзапроса
+    );
