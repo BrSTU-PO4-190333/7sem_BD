7f06faba79d18deeb28b2fafe81476d74fb5d749
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-11-21 15:40:13 +0300

    lab3/190333 : Sources added

--- lab3/sql/task6/6.sql
+++ lab3/sql/task6/6.sql
@@ -1,17 +1,43 @@
 SELECT
-    COUNT(B.id) AS КоличествоПациентовОбслужаныхЗаПериод,
-    C.de_name AS ГендерПациента,
-    D.de_name AS МестоОсмотра
+    Doctors.id AS ИдВрача,
+    CONCAT(
+        'Участок ',
+        Doctors.de_region,
+        ', кабиент ',
+        Doctors.de_office,
+        ', ',
+        Doctors.de_surname,
+        ', ',
+        Doctors.de_name,
+        ', ',
+        Doctors.de_patronymic
+    ) AS ДанныеВрача,
+    Genders.de_name AS ГендерПациента,
+    Places.de_name AS МестоОсмотра,
+    COUNT(Patients.id) AS КолвоПациентовОбслужЗаПериод
 FROM
-    DE_DOC_Inspection AS A,
-    DE_CTL_Patients AS B,
-    DE_CTL_Genders AS C,
-    DE_CTL_InspectionPlaces AS D
+    DE_DOC_Inspection AS Inspections
+    INNER JOIN DE_CTL_Patients AS Patients ON Patients.id = Inspections.de_patientId
+    INNER JOIN DE_CTL_Genders AS Genders ON Genders.id = Patients.de_genderId
+    INNER JOIN DE_CTL_InspectionPlaces AS Places ON Places.id = Inspections.de_placeId
+    INNER JOIN DE_CTL_Doctors AS Doctors ON Doctors.id = Inspections.de_doctorId
 WHERE
-    B.id = A.de_patientId
-    AND C.id = B.de_genderId
-    AND D.id = A.de_placeId
-    AND A.de_startTime > '2019-01-01 00:00:00' -- AND D.de_name = 'на дому'
-GROUP BY C.de_name, D.de_name
-ORDER BY
-    КоличествоПациентовОбслужаныхЗаПериод;
+    Inspections.de_startTime > '2019-01-01 00:00:00' -- AND D.de_name = 'на дому'
+GROUP BY
+    Doctors.id,
+    CONCAT(
+        'Участок ',
+        Doctors.de_region,
+        ', кабиент ',
+        Doctors.de_office,
+        ', ',
+        Doctors.de_surname,
+        ', ',
+        Doctors.de_name,
+        ', ',
+        Doctors.de_patronymic
+    ),
+    Genders.de_name,
+    Places.de_name
+ORDER BY ИдВрача
+LIMIT 24;
